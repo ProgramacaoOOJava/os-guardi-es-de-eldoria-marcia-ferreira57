86ed023a7e8b56567033530f6026352d1126d9ad
marcia-ferreira57 <marciacomarte@gmail.com>
2026-05-13 23:29:54 +0000

    desafio novato

--- Main.java
+++ Main.java
@@ -2,9 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
 
-        .
-        .
-        .
+       // Exibindo os dados para verificar se o ajuste funcionou
+        heroi1.exibirStatus();
 
     }
 }
--- Personagem.java
+++ Personagem.java
@@ -1,24 +1,28 @@
+//Desafio Novato
+
 public class Personagem {
     String nome;
     String classe;
     int nivel;
     int pontosDeVida;
     double poderBase;
-
+ 
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
+       this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
 
     }
 
     public void exibirStatus() {
-        System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
-
+       System.out.println("--- Status do Personagem ---");
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Classe: " + this.classe);
+        System.out.println("Nível: " + this.nivel);
+        System.out.println("Vida: " + this.pontosDeVida);
+        System.out.println("Força: " + this.poderBase);
     }
 }
 
